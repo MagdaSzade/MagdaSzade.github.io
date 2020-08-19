0b0a6e6e8cc734a625d0ad33b10065322dbb7de6
MagdaSzade <magdalenasuchanek@gmail.com>
2020-08-19 10:59:38 +0200

    next try

---
+++ precache-manifest.a932c702d4f20bb8d8f0cc6549627037.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3e7127a357da101f207b235699841027",
+    "url": "/index.html"
+  },
+  {
+    "revision": "d0acfaf9d1df2bd224db",
+    "url": "/static/css/main.c3a09f2e.chunk.css"
+  },
+  {
+    "revision": "c101f71d6af8610c37f3",
+    "url": "/static/js/2.55031831.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.55031831.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d0acfaf9d1df2bd224db",
+    "url": "/static/js/main.622a58aa.chunk.js"
+  },
+  {
+    "revision": "9b4e396c183e42c1fa5c",
+    "url": "/static/js/runtime-main.09b85ec0.js"
+  }
+]);
